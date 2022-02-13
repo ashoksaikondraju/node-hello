dcb4adbafeff5cbadb132e6a59f342385bac3185
ashoksaikondraju <95737486+ashoksaikondraju@users.noreply.github.com>
2022-02-13 18:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello surya !\n'
+  const msg = 'Hello surya!\n'
   res.end(msg);
 });
 
